fa3aca39428a82fe6b386aaf0c8d70e1a73777a6
jitendram0506 <jitendram0506@gmail.com>
2025-11-27 19:45:29 +0530

    Add files via upload
    Initial commit

---
+++ online-sales/src/main/java/com/org/sales/order/OnlineSalesApplication.java
@@ -0,0 +1,13 @@
+package com.org.sales.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineSalesApplication.class, args);
+	}
+
+}
